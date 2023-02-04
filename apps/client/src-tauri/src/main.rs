2ec9bed868d4aa6bd7aba36f3225e443f140e201
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-02-04 22:08:07 +0900

    feat: added a splashscreen

--- apps/client/src-tauri/src/main.rs
+++ apps/client/src-tauri/src/main.rs
@@ -3,8 +3,20 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+
+  window.get_window("main").unwrap().show().unwrap();
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
