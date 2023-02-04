61236478bf0fb1a4dbe1f0237297428a2c2e262d
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-02-04 22:08:07 +0900

    feat: added a splashscreen

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,14 @@
+import { invoke } from '@tauri-apps/api';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>,
-);
+setTimeout(() => {
+   invoke('close_splashscreen');
+
+   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+      <React.StrictMode>
+         <App />
+      </React.StrictMode>,
+   );
+}, 5000);
