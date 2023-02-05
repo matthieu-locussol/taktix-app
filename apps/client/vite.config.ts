4af1960ea259f0eb698baee05c992dad9e3ad834
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-02-05 12:22:24 +0900

    feat: updated vite config

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,4 +3,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
    plugins: [react()],
+   clearScreen: false,
+   server: {
+      strictPort: true,
+   },
+   envPrefix: ['VITE_', 'TAURI_'],
+   build: {
+      target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+      minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+      sourcemap: !!process.env.TAURI_DEBUG,
+   },
 });
