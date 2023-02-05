07b68683621ae3ab1a8fc92e757f77f5a5570c59
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-02-05 22:22:10 +0900

    feat: added mui & a chatbox to the client

--- apps/client/src/store/Store.ts
+++ apps/client/src/store/Store.ts
@@ -1,11 +1,14 @@
 import { makeAutoObservable } from 'mobx';
 import { CharacterStore } from './CharacterStore';
+import { ChatStore } from './ChatStore';
 import { LoadingScreenStore } from './LoadingScreenStore';
 
 export class Store {
    loadingScreenStore = new LoadingScreenStore();
 
    characterStore = new CharacterStore();
+
+   chatStore = new ChatStore();
 
    constructor() {
       makeAutoObservable(this);
