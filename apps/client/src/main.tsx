fbfc750a2a05dfe986c4acd4455ecf815b22ac52
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-02-05 15:34:42 +0900

    feat: updated icons & README

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,14 +1,9 @@
-import { invoke } from '@tauri-apps/api';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-setTimeout(() => {
-   invoke('close_splashscreen');
-
-   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-      <React.StrictMode>
-         <App />
-      </React.StrictMode>,
-   );
-}, 5000);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+);
