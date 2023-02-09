31cb859fad8dcfbfcfc6c2eae26a1154e461192b
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-02-09 16:50:17 +0900

    chore: bump version

--- apps/client/src/store/Store.ts
+++ apps/client/src/store/Store.ts
@@ -1,4 +1,8 @@
 import { makeAutoObservable } from 'mobx';
+import { ClientPacket } from 'shared';
+import { zServerPacket } from 'shared/src/client/ServerPacket';
+import { handleServerMessage } from '../handlers/handleServerMessage';
+import { handleServerResponse } from '../handlers/handleServerResponse';
 import { CharacterStore } from './CharacterStore';
 import { ChatStore } from './ChatStore';
 import { LoadingScreenStore } from './LoadingScreenStore';
@@ -14,5 +18,54 @@ export class Store {
 
    constructor() {
       makeAutoObservable(this);
+   }
+
+   setSocket(socket: WebSocket) {
+      this.socket = socket;
+
+      socket.onopen = () => {
+         const packet: ClientPacket = {
+            type: 'message',
+            packet: {
+               type: 'login',
+               data: {
+                  name: this.characterStore.name,
+               },
+            },
+         };
+
+         socket.send(JSON.stringify(packet));
+      };
+
+      socket.onmessage = (event) => {
+         try {
+            const { type, packet } = zServerPacket.parse(JSON.parse(event.data.toString()));
+
+            switch (type) {
+               case 'message': {
+                  const response = handleServerMessage(packet);
+                  const payload: ClientPacket = {
+                     type: 'response',
+                     packet: response,
+                  };
+                  console.log(`Sending a ${payload.packet.type} response...`);
+                  socket.send(JSON.stringify(payload));
+                  break;
+               }
+               case 'response': {
+                  handleServerResponse(packet);
+                  break;
+               }
+               default:
+                  throw new Error(`Unknown ServerPacket type: "${type}"`);
+            }
+         } catch (e) {
+            console.error(e);
+         }
+      };
+
+      socket.onerror = (error) => {
+         console.error(error);
+      };
    }
 }
--- apps/client/src/ui/SocketDiv.tsx
+++ apps/client/src/ui/SocketDiv.tsx
@@ -1,65 +1,12 @@
-import { useEffect, useMemo } from 'react';
-import { ClientPacket } from 'shared';
-import { zServerPacket } from 'shared/src/client/ServerPacket';
-import { handleServerMessage } from '../handlers/handleServerMessage';
-import { handleServerResponse } from '../handlers/handleServerResponse';
-import { store, useStore } from '../store';
+import { memo, useEffect } from 'react';
+import { store } from '../store';
 
-export const SocketDiv = () => {
-   const {
-      characterStore: { name },
-   } = useStore();
-
-   const socket = useMemo(() => new WebSocket(import.meta.env.VITE_SERVER_WEBSOCKET_URL), []);
-
+export const SocketDiv = memo(() => {
    useEffect(() => {
-      store.socket = socket;
+      if (store.socket === null) {
+         store.setSocket(new WebSocket(import.meta.env.VITE_SERVER_WEBSOCKET_URL));
+      }
    }, []);
 
-   socket.onopen = () => {
-      const packet: ClientPacket = {
-         type: 'message',
-         packet: {
-            type: 'login',
-            data: {
-               name,
-            },
-         },
-      };
-
-      socket.send(JSON.stringify(packet));
-   };
-
-   socket.onmessage = (event) => {
-      try {
-         const { type, packet } = zServerPacket.parse(JSON.parse(event.data.toString()));
-
-         switch (type) {
-            case 'message': {
-               const response = handleServerMessage(packet);
-               const payload: ClientPacket = {
-                  type: 'response',
-                  packet: response,
-               };
-               console.log(`Sending a ${payload.packet.type} response...`);
-               socket.send(JSON.stringify(payload));
-               break;
-            }
-            case 'response': {
-               handleServerResponse(packet);
-               break;
-            }
-            default:
-               throw new Error(`Unknown ServerPacket type: "${type}"`);
-         }
-      } catch (e) {
-         console.error(e);
-      }
-   };
-
-   socket.onerror = (error) => {
-      console.error(error);
-   };
-
    return <div />;
-};
+});
