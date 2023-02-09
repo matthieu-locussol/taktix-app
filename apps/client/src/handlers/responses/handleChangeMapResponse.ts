fdabaf373c4f48eedde3cd7a78488dc14fd12fea
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-02-09 16:37:17 +0900

    fix: handleChangeMapResponse error

--- apps/client/src/handlers/responses/handleChangeMapResponse.ts
+++ apps/client/src/handlers/responses/handleChangeMapResponse.ts
@@ -2,7 +2,7 @@ import { ChangeMapResponse } from 'shared';
 import { getCurrentScene } from '../../utils/game';
 
 export const handleChangeMapResponse = ({ data }: ChangeMapResponse): void => {
-   getCurrentScene().deleteAllExternalPlayers();
+   getCurrentScene()?.deleteAllExternalPlayers();
 
    data.players.forEach(({ name, posX, posY }) => {
       getCurrentScene().addExternalPlayer(name, { x: posX, y: posY });
