d1d72b01cc06cf85ad67c71b382ab83b2eb4700c
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2023-10-07 00:24:38 +0900

    feat: added register endpoint

--- apps/client/src/store/LoginStore.ts
+++ apps/client/src/store/LoginStore.ts
@@ -1,11 +1,15 @@
 import { makeAutoObservable } from 'mobx';
 
 export class LoginStore {
+   public email: string = '';
+
    public username: string = '';
 
    public password: string = '';
 
    public errorMessage: string = '';
+
+   public successMessage: string = '';
 
    public serverOnline: boolean = false;
 
@@ -13,8 +17,14 @@ export class LoginStore {
 
    public loading: boolean = false;
 
+   public mode: 'login' | 'register' = 'login';
+
    constructor() {
       makeAutoObservable(this);
+   }
+
+   setEmail(email: string) {
+      this.email = email;
    }
 
    setUsername(username: string) {
@@ -26,7 +36,13 @@ export class LoginStore {
    }
 
    setErrorMessage(errorMessage: string) {
+      this.successMessage = '';
       this.errorMessage = errorMessage;
+   }
+
+   setSuccessMessage(successMessage: string) {
+      this.errorMessage = '';
+      this.successMessage = successMessage;
    }
 
    setServerOnline(serverOnline: boolean) {
@@ -41,7 +57,33 @@ export class LoginStore {
       this.loading = loading;
    }
 
+   setMode(mode: 'login' | 'register') {
+      this.mode = mode;
+   }
+
+   switchPage() {
+      this.setMode(this.mode === 'login' ? 'register' : 'login');
+   }
+
+   resetFields() {
+      this.setEmail('');
+      this.setUsername('');
+      this.setPassword('');
+   }
+
    get canLogin() {
       return this.username !== '' && this.password !== '';
+   }
+
+   get canRegister() {
+      return this.email !== '' && this.username !== '' && this.password !== '';
+   }
+
+   get currentPage() {
+      return this.mode === 'login' ? 'Log In' : 'Register';
+   }
+
+   get otherPage() {
+      return this.mode === 'login' ? 'Register' : 'Log In';
    }
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from './data/teleportationSpots';
 export * from './packets/ClientPacket';
 export * from './packets/ServerPacket';
+export * from './schemas/RegisterSchema';
 export * from './types/Player';
 export * from './types/SceneData';
 export * from './utils/_assert';
