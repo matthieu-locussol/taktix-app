1333aa2b4bfc2bc68140fb341287bdec1a8117a4
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-01-21 20:21:22 +0900

    refactor: removed useless handlers on client side

--- apps/client/src/handlers/handlePlayerMove.ts
+++
@@ -1,6 +0,0 @@
-import { ServerPacketType } from 'shared/src/packets/ServerPacket';
-import { Store } from '../store/Store';
-
-export const handlePlayerMove = (_: ServerPacketType<'playerMove'>, _store: Store) => {
-   // store.gameStore.getCurrentScene.moveExternalPlayer(name, x, y);
-};
