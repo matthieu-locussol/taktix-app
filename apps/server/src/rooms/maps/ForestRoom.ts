71c24d735f381fdb86c2dc327be1346a65222191
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-01-26 00:07:39 +0900

    feat: added Forest map poggers

---
+++ apps/server/src/rooms/maps/ForestRoom.ts
@@ -0,0 +1,7 @@
+import { MapRoom } from '../MapRoom';
+
+export class ForestRoom extends MapRoom {
+   constructor() {
+      super('ForestRoom');
+   }
+}
--- packages/shared/src/types/Room.ts
+++ packages/shared/src/types/Room.ts
@@ -1,3 +1,3 @@
 // This file has been automatically generated. DO NOT edit it manually.
 
-export type Room = 'CloudsRoom' | 'DungeonRoom' | 'HouseRoom';
+export type Room = 'CloudsRoom' | 'DungeonRoom' | 'ForestRoom' | 'HouseRoom';
