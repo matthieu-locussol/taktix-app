f1838059b8054e93df7b9dccdaf619088a71f593
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-01-27 17:06:31 +0900

    feat: added TimeMgt

---
+++ packages/shared/src/utils/timeMgt.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest';
+import { TimeMgt } from './timeMgt';
+
+describe('TimeMgt', () => {
+   describe('wait', async () => {
+      it('should wait for the specified duration', async () => {
+         const start = Date.now();
+         await TimeMgt.wait(100);
+         const end = Date.now();
+
+         expect(end - start).toBeGreaterThanOrEqual(100);
+      });
+   });
+});
