3a48f76f6e9547483fd66d02e5d71282a21a98ff
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-01-28 13:02:01 +0900

    feat: set fps limit to 60fps

--- apps/client/src/game/PhaserGame.ts
+++ apps/client/src/game/PhaserGame.ts
@@ -3,12 +3,20 @@ import Phaser from 'phaser';
 import { store } from '../store';
 import { mapsScenes } from './mapsScenes';
 
+const FPS = 60;
+
 export const config: Phaser.Types.Core.GameConfig = {
    type: Phaser.AUTO,
    backgroundColor: '#1f2937',
    parent: 'root-game',
    scene: [...mapsScenes],
    pixelArt: true,
+   fps: {
+      min: FPS,
+      limit: FPS,
+      target: FPS,
+      smoothStep: true,
+   },
    scale: {
       mode: Phaser.Scale.ScaleModes.ENVELOP,
       autoCenter: Phaser.Scale.Center.CENTER_BOTH,
