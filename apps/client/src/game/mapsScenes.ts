67f2ac9de86c869d97df73d303f0c12ca768ef69
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-01-28 23:21:39 +0900

    fix: AAA_InitialScene is set in mapsScenes too

--- apps/client/src/game/mapsScenes.ts
+++ apps/client/src/game/mapsScenes.ts
@@ -1,11 +1,13 @@
 // This file has been automatically generated. DO NOT edit it manually.
 
+import { AAA_InitialScene } from './scenes/AAA_InitialScene';
 import { CloudsScene } from './scenes/CloudsScene';
 import { DungeonScene } from './scenes/DungeonScene';
 import { ForestScene } from './scenes/ForestScene';
 import { HouseScene } from './scenes/HouseScene';
 
 export const mapsScenes: Phaser.Types.Scenes.SceneType[] = [
+   AAA_InitialScene,
    CloudsScene,
    DungeonScene,
    ForestScene,
