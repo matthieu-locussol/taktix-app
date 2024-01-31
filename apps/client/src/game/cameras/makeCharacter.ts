61bfaab8a1b0bfd74acbfc06f9e5201f38e3b69f
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-01-31 21:12:23 +0900

    refactor: set name + square in a player wrapper to always be on top of layers

--- apps/client/src/game/cameras/makeCharacter.ts
+++ apps/client/src/game/cameras/makeCharacter.ts
@@ -16,34 +16,33 @@ export const makeCharacter = (scene: Scene, name: string, isPlayer: boolean) => 
    characterSprite.scale = SCALE_FACTOR;
 
    const offsetX = (CHARACTER_WIDTH * SCALE_FACTOR - name.length * CHARACTER_LETTER_WIDTH) / 2;
-   const characterName = scene.add.text(offsetX, -CHARACTER_LETTER_WIDTH, name, {
-      align: 'center',
-      fontSize: 6,
-      fontFamily: 'Orbitron',
-      resolution: 4,
-      shadow: {
-         offsetX: 0,
-         offsetY: 0,
-         color: '#000000',
-         blur: 2,
-         stroke: true,
-         fill: true,
-      },
-   });
+   const characterName = scene.add
+      .text(offsetX, 0, name, {
+         align: 'center',
+         fontSize: 6,
+         fontFamily: 'Orbitron',
+         resolution: 4,
+         shadow: {
+            offsetX: 0,
+            offsetY: 0,
+            color: '#000000',
+            blur: 2,
+            stroke: true,
+            fill: true,
+         },
+      })
+      .setDepth(999);
    characterName.scale = SCALE_FACTOR;
 
    const characterSquare = scene.add
-      .graphics({
-         fillStyle: { color: isPlayer ? 0xff0000 : 0x00ff00 },
-      })
-      .fillRect(0, 0, TILE_SIZE * SCALE_FACTOR, TILE_SIZE * SCALE_FACTOR)
+      .rectangle(
+         0,
+         0,
+         TILE_SIZE * SCALE_FACTOR,
+         TILE_SIZE * SCALE_FACTOR,
+         isPlayer ? 0xff0000 : 0x00ff00,
+      )
       .setDepth(999);
-
-   const externalPlayerContainer = scene.add.container(0, 0, [
-      characterName,
-      characterSprite,
-      characterSquare,
-   ]);
 
    if (scene.minimap !== null) {
       scene.minimap.ignore(characterName);
@@ -52,5 +51,11 @@ export const makeCharacter = (scene: Scene, name: string, isPlayer: boolean) => 
 
    scene.cameras.main.ignore(characterSquare);
 
-   return { sprite: characterSprite, container: externalPlayerContainer };
+   return {
+      sprite: characterSprite,
+      wrapper: {
+         name: characterName,
+         square: characterSquare,
+      },
+   };
 };
