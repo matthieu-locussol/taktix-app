dec564347e31dcf0fc499aa93aa92a8489efec3a
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-02-02 16:49:51 +0900

    feat: added ChatSettings buttons

--- apps/client/src/ui/hud/Chatbox.tsx
+++ apps/client/src/ui/hud/Chatbox.tsx
@@ -1,9 +1,13 @@
+import PlusIcon from '@mui/icons-material/AddRounded';
+import MinusIcon from '@mui/icons-material/RemoveRounded';
+import ChannelIcon from '@mui/icons-material/SettingsRounded';
 import { darken, styled, useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { observer } from 'mobx-react-lite';
 import { useEffect, useRef } from 'react';
 import { useStore } from '../../store';
+import { SmallButton } from './components/SmallButton';
 
 export const Chatbox = observer(() => {
    const theme = useTheme();
@@ -30,44 +34,57 @@ export const Chatbox = observer(() => {
 
    return (
       <Root component="form" onSubmit={sendMessage} widthPercent={hudStore.chatboxWidth}>
-         <Chat
-            ref={chatboxRef}
-            heightPercent={hudStore.chatboxHeight}
-            inputHeight={hudStore.chatboxInputHeight}
-         >
-            {chatStore.messages.map(({ author, content, channel }, idx) =>
-               chatStore.isSystemChannel(channel) ? (
-                  <Typography
-                     key={idx}
-                     fontStyle="italic"
-                     fontSize="1vw"
-                     lineHeight="2.5vh"
-                     color={theme.palette.channels[channel]}
-                  >
-                     {content}
-                  </Typography>
-               ) : (
-                  <Typography
-                     key={idx}
-                     fontSize="1vw"
-                     lineHeight="2.5vh"
-                     color={theme.palette.channels[channel]}
-                  >
-                     {author}: {content}
-                  </Typography>
-               ),
-            )}
-         </Chat>
-         <ChatInput
-            ref={inputRef}
-            value={chatStore.input}
-            onFocus={() => gameStore.enableKeyboard(false)}
-            onBlur={() => gameStore.enableKeyboard(true)}
-            onChange={(e) => chatStore.setInput(e.target.value)}
-            maxLength={160}
-            widthPercent={hudStore.chatboxWidth}
-            inputHeight={hudStore.chatboxInputHeight}
-         />
+         <Wrapper widthPercent={hudStore.chatboxWidth}>
+            <Chat
+               ref={chatboxRef}
+               heightPercent={hudStore.chatboxHeight}
+               inputHeight={hudStore.chatboxInputHeight}
+            >
+               {chatStore.messages.map(({ author, content, channel }, idx) =>
+                  chatStore.isSystemChannel(channel) ? (
+                     <Typography
+                        key={idx}
+                        fontStyle="italic"
+                        fontSize="min(1vw, 1.5vh)"
+                        lineHeight="2vh"
+                        color={theme.palette.channels[channel]}
+                     >
+                        {content}
+                     </Typography>
+                  ) : (
+                     <Typography
+                        key={idx}
+                        fontSize="min(1vw, 1.5vh)"
+                        lineHeight="2vh"
+                        color={theme.palette.channels[channel]}
+                     >
+                        {author}: {content}
+                     </Typography>
+                  ),
+               )}
+            </Chat>
+            <ChatInput
+               ref={inputRef}
+               value={chatStore.input}
+               onFocus={() => gameStore.enableKeyboard(false)}
+               onBlur={() => gameStore.enableKeyboard(true)}
+               onChange={(e) => chatStore.setInput(e.target.value)}
+               maxLength={160}
+               widthPercent={hudStore.chatboxWidth}
+               inputHeight={hudStore.chatboxInputHeight}
+            />
+         </Wrapper>
+         <ChatSettings>
+            <SmallButton>
+               <PlusIcon fontSize="inherit" />
+            </SmallButton>
+            <SmallButton>
+               <MinusIcon fontSize="inherit" />
+            </SmallButton>
+            <SmallButton>
+               <ChannelIcon fontSize="inherit" />
+            </SmallButton>
+         </ChatSettings>
       </Root>
    );
 });
@@ -83,13 +100,23 @@ interface ChatStyleProps {
 }
 
 const Root = styled(Box, { shouldForwardProp: (prop) => prop !== 'widthPercent' })<StyleProps>(
-   ({ theme, widthPercent }) => ({
+   ({ widthPercent }) => ({
       position: 'absolute',
+      top: 8,
       left: 8,
       bottom: 8,
+      width: `calc(${widthPercent}vw - 16px)`,
+      boxSizing: 'border-box',
+      borderRadius: 8,
+   }),
+);
+
+const Wrapper = styled(Box, { shouldForwardProp: (prop) => prop !== 'widthPercent' })<StyleProps>(
+   ({ theme, widthPercent }) => ({
       border: `1px solid ${theme.palette.paper.border}`,
       background: darken(`${theme.palette.paper.background}C6`, 0.15),
-      width: `calc(${widthPercent}vw - 16px)`,
+      width: `calc(${widthPercent}vw - 16px - min(1.8vw, 3vh) - 0.5vw)`,
+      height: '100%',
       boxSizing: 'border-box',
       borderRadius: 8,
    }),
@@ -99,8 +126,8 @@ const Chat = styled(Box, {
    shouldForwardProp: (prop) => prop !== 'heightPercent' && prop !== 'inputHeight',
 })<ChatStyleProps>(({ heightPercent, inputHeight }) => ({
    padding: 8,
-   height: `calc(${heightPercent}vh - ${inputHeight}px - 36px - 2vh)`,
-   maxHeight: `calc(${heightPercent}vh - ${inputHeight}px - 36px - 2vh)`,
+   height: `calc(${heightPercent}vh - ${inputHeight}px - 36px - 1.5vh)`,
+   maxHeight: `calc(${heightPercent}vh - ${inputHeight}px - 36px - 1.5vh)`,
    overflowX: 'hidden',
    overflowY: 'scroll',
    color: 'white',
@@ -111,17 +138,31 @@ const Chat = styled(Box, {
 const ChatInput = styled('input', {
    shouldForwardProp: (prop) => prop !== 'widthPercent' && prop !== 'inputHeight',
 })<StyleProps>(({ theme, widthPercent, inputHeight }) => ({
-   padding: '1vh 1vw 1vh 1vw',
-   fontSize: '1vw',
+   padding: '0.75vh 0.5vw 0.75vh 0.5vw',
+   fontSize: 'min(1vw, 1.5vh)',
    border: 'none',
    borderTop: `2px solid ${theme.palette.paper.border}`,
    outline: 'none',
-   backgroundColor: '#E8E8E8',
+   backgroundColor: `${theme.palette.chalk.main}C6`,
    '&:hover': {
-      backgroundColor: '#DDDDDD',
+      backgroundColor: theme.palette.chalk.main,
+   },
+   '&:focus': {
+      backgroundColor: theme.palette.chalk.main,
    },
    borderBottomLeftRadius: 8,
    borderBottomRightRadius: 8,
-   width: `calc(${widthPercent}vw - 2vw - 18px)`,
+   width: `calc(${widthPercent}vw - 1vw - 18px - min(1.8vw, 3vh) - 0.5vw)`,
    height: `${inputHeight}px`,
 }));
+
+const ChatSettings = styled(Box)({
+   position: 'absolute',
+   top: 0,
+   right: 0,
+   bottom: 0,
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-between',
+   flexGrow: 1,
+});
