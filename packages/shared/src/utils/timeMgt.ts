1a755698a5fdba89b407b41b2485e05bc4108a8e
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-02-18 11:01:18 +0900

    fix: changelog generation

--- packages/shared/src/utils/timeMgt.ts
+++ packages/shared/src/utils/timeMgt.ts
@@ -6,10 +6,12 @@ export namespace TimeMgt {
 
    export const formatDatetime = (date: Date) => {
       const year = date.getFullYear();
-      const month = date.getMonth() + 1;
-      const day = date.getDate();
-      const hours = date.getHours();
-      const minutes = date.getMinutes();
+      const month = Number(date.getMonth() + 1)
+         .toString()
+         .padStart(2, '0');
+      const day = date.getDate().toString().padStart(2, '0');
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
 
       return `${year}-${month}-${day} @ ${hours}:${minutes}`;
    };
