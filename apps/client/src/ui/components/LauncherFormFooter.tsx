0868c9764aa742b5c9523305bc79713d18d3b4dd
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-02-29 16:21:45 +0900

    feat: launcher links

---
+++ apps/client/src/ui/components/LauncherFormFooter.tsx
@@ -0,0 +1,27 @@
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '../../store';
+import { useTranslation } from '../../types/react-i18next';
+import { LauncherLinks } from './LauncherLinks';
+
+export const LauncherFormFooter = observer(() => {
+   const { screenStore } = useStore();
+   const { t } = useTranslation();
+
+   return (
+      <Box
+         sx={{
+            mt: 'auto',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+         }}
+      >
+         <Link onClick={() => screenStore.switchBetweenLoginAndRegister()}>
+            {t(screenStore.loginOrRegisterOppositeName)}
+         </Link>
+         <LauncherLinks />
+      </Box>
+   );
+});
---
+++ apps/client/src/ui/components/LauncherLinks.tsx
@@ -0,0 +1,42 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import WebsiteIcon from '@mui/icons-material/LanguageRounded';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import { DiscordIcon } from './DiscordIcon';
+
+const LINKS = [
+   {
+      href: 'https://taktix.vercel.app',
+      icon: <WebsiteIcon />,
+      color: '#14B8A6',
+   },
+   {
+      href: 'https://discord.gg/9a9EVKTMkX',
+      icon: <DiscordIcon />,
+      color: '#5865F2',
+   },
+   {
+      href: 'https://github.com/matthieu-locussol/taktix-app',
+      icon: <GitHubIcon />,
+      color: '#adbac7',
+   },
+] as const;
+
+export const LauncherLinks = () => (
+   <Box>
+      {LINKS.map(({ href, icon, color }) => (
+         <IconButton
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            color="inherit"
+            sx={{
+               transition: 'color 0.2s',
+               '&:hover': { color },
+            }}
+         >
+            {icon}
+         </IconButton>
+      ))}
+   </Box>
+);
