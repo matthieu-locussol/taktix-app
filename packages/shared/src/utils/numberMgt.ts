4efe2cedbcd0b9e95a48e7be8826b20af814980d
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-04-14 19:11:17 +0900

    chore: added hexStringToNumber utility function

--- packages/shared/src/utils/numberMgt.ts
+++ packages/shared/src/utils/numberMgt.ts
@@ -1,7 +1,14 @@
+import { _assertTrue } from './_assert';
+
 export namespace NumberMgt {
    export const clamp = (value: number, min: number, max: number) =>
       Math.min(Math.max(value, min), max);
 
    export const random = (min: number, max: number) =>
       Math.floor(Math.random() * (max - min + 1) + min);
+
+   export const hexStringToNumber = (hexString: string): number => {
+      _assertTrue(/^#[0-9A-Fa-f]{6}$/.test(hexString), `Invalid hex string: '${hexString}'`);
+      return parseInt(hexString.slice(1), 16);
+   };
 }
