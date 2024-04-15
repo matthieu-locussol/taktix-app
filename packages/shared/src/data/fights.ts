f117f0f16d1d305c40d4ec1cf51ad44d3a62295f
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-04-15 17:59:03 +0900

    refactor: monsters data & fights data are handled separately

--- packages/shared/src/data/fights.ts
+++ packages/shared/src/data/fights.ts
@@ -1,60 +1,14 @@
 import { PvEFightParameters } from '../types/PvEFight';
-import { WeaponType } from '../types/Weapon';
-import { StatisticMgt } from '../utils/statisticMgt';
+import { monsters } from './monsters';
 
 export const getMonstersInformations = (
    monsterGroupId: number,
 ): PvEFightParameters['monstersInformations'] => {
    if (monsterGroupId === 1) {
       return [
-         {
-            name: 'Character 1',
-            health: 50,
-            magicShield: 0,
-            level: 1,
-            experience: 50,
-            weaponType: WeaponType.Axe1H,
-            weaponDamages: [{ type: 'strength', min: 1, max: 2 }],
-            rawStatistics: StatisticMgt.serializeStatistics(
-               StatisticMgt.makeMockedStatistics({
-                  'strength_+f': 10,
-               }),
-            ),
-            talents: [],
-            uniquesPowers: [],
-         },
-         {
-            name: 'Character 2',
-            health: 10,
-            magicShield: 0,
-            level: 1,
-            experience: 5,
-            weaponType: WeaponType.Axe1H,
-            weaponDamages: [{ type: 'dexterity', min: 2, max: 4 }],
-            rawStatistics: StatisticMgt.serializeStatistics(
-               StatisticMgt.makeMockedStatistics({
-                  'dexterity_+f': 10,
-               }),
-            ),
-            talents: [],
-            uniquesPowers: [],
-         },
-         {
-            name: 'Character 3',
-            health: 30,
-            magicShield: 0,
-            level: 1,
-            experience: 5,
-            weaponType: WeaponType.Axe1H,
-            weaponDamages: [{ type: 'strength', min: 1, max: 2 }],
-            rawStatistics: StatisticMgt.serializeStatistics(
-               StatisticMgt.makeMockedStatistics({
-                  'strength_+f': 10,
-               }),
-            ),
-            talents: [],
-            uniquesPowers: [],
-         },
+         monsters['enemy:nono']({ level: 2 }),
+         monsters['enemy:nono']({ level: 1 }),
+         monsters['enemy:nono']({ level: 1 }),
       ];
    }
 
---
+++ packages/shared/src/data/monsters.ts
@@ -0,0 +1,37 @@
+import { PvEFighterInformations } from '../types/PvEFight';
+import { WeaponType } from '../types/Weapon';
+import { StatisticMgt } from '../utils/statisticMgt';
+import { TranslationKey } from './translations';
+
+type FilterEnemy<T> = T extends `enemy:${infer EnemyName}` ? `enemy:${EnemyName}` : never;
+type MonsterName = FilterEnemy<TranslationKey>;
+type MonsterGenerator = (parameters: {
+   level: number;
+}) => PvEFighterInformations & { name: MonsterName };
+
+const monstersArray: MonsterGenerator[] = [
+   ({ level }) => ({
+      name: 'enemy:nono',
+      health: 80 + level * 15,
+      magicShield: 30 + level * 5,
+      level,
+      experience: 30 + level * 25,
+      weaponType: WeaponType.Axe1H,
+      weaponDamages: [{ type: 'strength', min: level, max: level * 3 }],
+      rawStatistics: StatisticMgt.serializeStatistics(
+         StatisticMgt.makeMockedStatistics({
+            'strength_+f': level * 8,
+         }),
+      ),
+      talents: [],
+      uniquesPowers: [],
+   }),
+] as const;
+
+export const monsters = monstersArray.reduce<Record<MonsterName, MonsterGenerator>>(
+   (acc, monster) => ({
+      ...acc,
+      [monster({ level: 1 }).name]: monster,
+   }),
+   {} as Record<MonsterName, MonsterGenerator>,
+);
