fcccac3a75b8f8c17fac2fd50b0d69b1c0abd934
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-04-21 15:03:05 +0900

    chore: added more reserved names

--- packages/shared/src/utils/stringMgt.ts
+++ packages/shared/src/utils/stringMgt.ts
@@ -1,7 +1,26 @@
+import { INTERNAL_PLAYER_NAME } from '../types/Player';
+
 export namespace StringMgt {
    export const isCharacterNameValid = (characterName: string): boolean =>
       /^[a-zA-Z0-9_-]{2,15}$/.test(characterName);
 
    export const toUpperCaseFirst = (str: string): string =>
       str.charAt(0).toUpperCase() + str.slice(1);
+
+   export const isReservedName = (name: string): boolean => {
+      const reservedNames = [
+         INTERNAL_PLAYER_NAME,
+         'SERVER',
+         'ERROR',
+         'GENERAL',
+         'TRADE',
+         'PRIVATE',
+         'PLAYER',
+         'ADMIN',
+         'MODERATOR',
+      ].map((name) => name.toLowerCase());
+
+      const lowerCaseName = name.toLowerCase();
+      return reservedNames.includes(lowerCaseName);
+   };
 }
