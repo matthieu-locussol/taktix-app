b1162b96bec8ea20c2a7e6f3dc78106afc86c08f
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-04-24 22:13:03 +0900

    refactor: rust files formatting

--- apps/client/src-tauri/src/main.rs
+++ apps/client/src-tauri/src/main.rs
@@ -5,40 +5,40 @@ use discord::DiscordState;
 use tauri::Manager;
 
 fn main() {
-   let app = tauri::Builder
-      ::default()
-      .manage(DiscordState::new())
-      .invoke_handler(tauri::generate_handler![discord::set_discord_rich_presence])
-      .build(tauri::generate_context!())
-      .expect("An error occurred while running the application");
+    let app = tauri::Builder
+        ::default()
+        .manage(DiscordState::new())
+        .invoke_handler(tauri::generate_handler![discord::set_discord_rich_presence])
+        .build(tauri::generate_context!())
+        .expect("An error occurred while running the application");
 
-   app.run(|app_handle, event| {
-      match event {
-         tauri::RunEvent::Updater(updater_event) => {
-            let main_window = app_handle.get_window("main");
+    app.run(|app_handle, event| {
+        match event {
+            tauri::RunEvent::Updater(updater_event) => {
+                let main_window = app_handle.get_window("main");
 
-            match updater_event {
-               tauri::UpdaterEvent::DownloadProgress { chunk_length, content_length } => {
-                  if main_window.is_some() && content_length.is_some() {
-                     let content_length: u64 = content_length.unwrap();
-                     if content_length > 0 {
-                        if
-                           let Err(err) = main_window
-                              .unwrap()
-                              .emit(
-                                 "updateProgress",
-                                 (chunk_length as f32) / (content_length as f32)
-                              )
-                        {
-                           println!("{}", err);
+                match updater_event {
+                    tauri::UpdaterEvent::DownloadProgress { chunk_length, content_length } => {
+                        if main_window.is_some() && content_length.is_some() {
+                            let content_length: u64 = content_length.unwrap();
+                            if content_length > 0 {
+                                if
+                                    let Err(err) = main_window
+                                        .unwrap()
+                                        .emit(
+                                            "updateProgress",
+                                            (chunk_length as f32) / (content_length as f32)
+                                        )
+                                {
+                                    println!("{}", err);
+                                }
+                            }
                         }
-                     }
-                  }
-               }
-               _ => {}
+                    }
+                    _ => {}
+                }
             }
-         }
-         _ => {}
-      }
-   });
+            _ => {}
+        }
+    });
 }
