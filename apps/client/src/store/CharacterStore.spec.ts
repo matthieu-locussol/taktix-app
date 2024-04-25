e7ee227bec7e718c108ea4b56ffed4fe1646e944
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-04-25 13:46:08 +0900

    test: added Store mock to CharacterStore

--- apps/client/src/store/CharacterStore.spec.ts
+++ apps/client/src/store/CharacterStore.spec.ts
@@ -1,11 +1,17 @@
 import { ProfessionType } from 'shared/src/types/Profession';
 import { StatisticMgt } from 'shared/src/utils/statisticMgt';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { CharacterStore } from './CharacterStore';
+import { Store } from './Store';
+
+vi.mock('./Store', () => {
+   const MockedStore = vi.fn().mockImplementation(() => ({}));
+   return { Store: MockedStore };
+});
 
 describe('CharacterStore', () => {
    it('should be initialized', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
 
       expect(store).toBeDefined();
       expect(store.map).toEqual('AAA_InitialRoom');
@@ -20,55 +26,55 @@ describe('CharacterStore', () => {
    });
 
    it('should set the map', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setMap('CloudsRoom');
       expect(store.map).toBe('CloudsRoom');
    });
 
    it('should set the name', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setName('John');
       expect(store.name).toBe('John');
    });
 
    it('should set the profession', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setProfession(ProfessionType.Mage);
       expect(store.profession).toBe(ProfessionType.Mage);
    });
 
    it('should set the position', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setPosition({ x: 1, y: 2 });
       expect(store.position).toEqual({ x: 1, y: 2 });
    });
 
    it('should set the position x', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setPositionX(1);
       expect(store.position).toEqual({ x: 1, y: 0 });
    });
 
    it('should set the position y', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setPositionY(2);
       expect(store.position).toEqual({ x: 0, y: 2 });
    });
 
    it('should set the talents', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setTalents([1, 2, 3]);
       expect(store.talents).toEqual([1, 2, 3]);
    });
 
    it('should set the talents points', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setTalentsPoints(5);
       expect(store.talentsPoints).toBe(5);
    });
 
    it('should set the base statistics', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setBaseStatistics(StatisticMgt.makeMockedStatistics({ 'strength_+f': 10 }));
       expect(store.baseStatistics).toEqual(
          StatisticMgt.makeMockedStatistics({ 'strength_+f': 10 }),
@@ -76,7 +82,7 @@ describe('CharacterStore', () => {
    });
 
    it('should set the base statistics points', () => {
-      const store = new CharacterStore();
+      const store = new CharacterStore(new Store());
       store.setBaseStatisticsPoints(5);
       expect(store.baseStatisticsPoints).toBe(5);
    });
