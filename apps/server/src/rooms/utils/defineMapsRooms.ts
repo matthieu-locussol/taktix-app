af9da6c58a04b932cd5a3466fbaa5a6924097308
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-05-16 12:58:43 +0900

    feat: Moonshadow shop

--- apps/server/src/rooms/utils/defineMapsRooms.ts
+++ apps/server/src/rooms/utils/defineMapsRooms.ts
@@ -3,10 +3,12 @@ import { MoonshadowBarRoom } from '../maps/MoonshadowBarRoom';
 import { MoonshadowHamletRoom } from '../maps/MoonshadowHamletRoom';
 import { MoonshadowHotelRoom } from '../maps/MoonshadowHotelRoom';
 import { MoonshadowInnRoom } from '../maps/MoonshadowInnRoom';
+import { MoonshadowShopRoom } from '../maps/MoonshadowShopRoom';
 
 export const defineMapsRooms = (gameServer: Server) => {
    gameServer.define('MoonshadowBarRoom', MoonshadowBarRoom);
    gameServer.define('MoonshadowHamletRoom', MoonshadowHamletRoom);
    gameServer.define('MoonshadowHotelRoom', MoonshadowHotelRoom);
    gameServer.define('MoonshadowInnRoom', MoonshadowInnRoom);
+   gameServer.define('MoonshadowShopRoom', MoonshadowShopRoom);
 };
